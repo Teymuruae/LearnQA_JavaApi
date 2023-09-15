4dfa554f16a133684c05b3a277915f2de200d5f2
Teymur <baratov.teymur@yandex.ru>
2023-09-15 22:13:20 +0300

    homework 5 done

---
+++ src/test/java/HomeWork5JsonParse.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class HomeWork5JsonParse {
+
+    @Test
+    void jsonParseTest() {
+        Map<String, ?> map  = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath().get("messages[1]");
+
+        System.out.println(map);
+    }
+}
