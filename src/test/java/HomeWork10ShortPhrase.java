08defd6ebf652d7b6cbf7de2e817f8cb62605aef
Teymur <baratov.teymur@yandex.ru>
2023-09-23 23:30:29 +0300

    homework 9 done

---
+++ src/test/java/HomeWork10ShortPhrase.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class HomeWork10ShortPhrase {
+
+    private static final String
+            less15 = "one two",
+            exact15 = "one two three f",
+            greaterThan15 = "one two three four five",
+            empty = "";
+
+    public void checkStringLength(String valueToCheck) {
+        Assertions.assertTrue(valueToCheck.length() >= 15, "Length is less, than 15");
+    }
+
+    @ValueSource(strings = {less15, exact15, greaterThan15, empty})
+    @ParameterizedTest
+    void stringLengthTest(String value) {
+        checkStringLength(value);
+    }
+}
