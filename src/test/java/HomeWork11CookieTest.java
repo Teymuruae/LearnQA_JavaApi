93a48026bd5914d1d2ad3bbddc8d87a8459c86e8
Teymur <baratov.teymur@yandex.ru>
2023-09-24 14:53:00 +0300

    homework 11

--- src/test/java/HomeWork11CookieTest.java
+++ src/test/java/HomeWork11CookieTest.java
@@ -2,21 +2,26 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.Map;
 
 public class HomeWork11CookieTest {
 
+    private String
+            url = "https://playground.learnqa.ru/api/homework_cookie",
+            cookieKey = "HomeWork",
+            cookieValue = "hw_value";
+
     @Test
     void cookieTest() {
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/homework_cookie")
-                .then().log().all().extract().response();
-        Map<String, String> cookies = response.getCookies();
-        Assertions.assertTrue(cookies.containsKey("HomeWork"), "Response hasn't cookie 'HomeWork'");
+                .get(url);
 
-        String actualCookie = response.getCookie("HomeWork");
-        String expectedCookie = "hw_value";
-        System.out.println(actualCookie);
+        Map<String, String> cookies = response.getCookies();
+        Assertions.assertTrue(cookies.containsKey(cookieKey), "Response hasn't cookie 'HomeWork'");
+
+        String actualCookie = response.getCookie(cookieKey);
+        String expectedCookie = cookieValue;
         Assertions.assertEquals(expectedCookie, actualCookie, "response cookie is not equals 'hw_value'");
     }
 }
